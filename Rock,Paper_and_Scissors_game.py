5eaf8c72520c4b6017d33a4d206a37d0ec8e5f9b
MD FORIDUL IBNE QAUSER <foridulislam35962@gmail.com>
2025-02-15 12:56:58 +0600

    Rock,Paper_and_Scissors_game.py

--- Rock,Paper_and_Scissors_game.py
+++ Rock,Paper_and_Scissors_game.py
@@ -1,5 +1,8 @@
 import random
 import time
+import termcolor
+import pyfiglet
+
 def score(mp,cp,manc,comc):
     print("Your choice is:",manc)
     print("Computer choice is: ",comc)
@@ -12,19 +15,25 @@ def wtl(mp,cp):
 
 def choice():
     print("Enter any number by your choiceing:")
-    print("Enter 1 for: Rock")
-    print("Enter 2 for: Paper")
-    print("Enter 3 for: Scissors")
+    print("Enter 1 for: ",termcolor.colored("Rock",'green'))
+    print("Enter 2 for: ",termcolor.colored("Paper",'red'))
+    print("Enter 3 for: ",termcolor.colored("Scissors",'blue'))
     a=int(input("Enter your choice number: "))
     return a
 
-
-print("\t\tWelcome to Rock, Paper and Scissors game")
+ban=termcolor.colored(pyfiglet.figlet_format("Welcome to Game"),'green')
+print(ban)
+ban=termcolor.colored(pyfiglet.figlet_format("Of"),'red')
+print(ban)
+ban=termcolor.colored(pyfiglet.figlet_format("Rock Paper Scissors"),'blue')
+print(ban)
 time.sleep(2.0)
 t=int(input("Enter how many point do you want to play: "))
+
 mp=0
 cp=0
 l=0
+
 while mp<t and cp<t:
     time.sleep(2.0)
     if(l==0):
@@ -32,9 +41,15 @@ while mp<t and cp<t:
         l+=1
     else:
         a=int(input("Again enter your choice number: "))
+    
+    if(a>3 or a<1 ):
+        print("You enter wrong choice. please try again.")
+        continue
+
     ch=['Rock','Paper','Scissors']
     manc=ch[a-1]
     comc=random.choice(ch)
+    
     if(manc==comc):
         score(mp,cp,manc,comc)
     elif(manc=="Rock" and comc=="Scissors") or \
@@ -47,12 +62,9 @@ while mp<t and cp<t:
         score(mp,cp,manc,comc)
 
 if(mp==cp):
-    wtl(mp,cp)
-    print("\n\tThe match is tie.")
+    print("\n\tThe match is {}.".format(termcolor.colored("Tie",'blue')))
 elif(mp>cp):
-    wtl(mp,cp)
-    print("\n\tYou Win the match")
+    print("\n\tYou {} the match".format(termcolor.colored("Win",'green')))
 else:
-    wtl(mp,cp)
-    print("\n\tYou lose the match")
+    print("\n\tYou {} the match".format(termcolor.colored("Lose",'red')))
 print("\n\t\tGame Over")
